e3d2bfe528f66972e8140729df6184a23f12c374
Keith Roe <kdroe1@gmail.com>
2023-03-15 14:36:38 -0400

    Variables Exercise

---
+++ script.js
@@ -0,0 +1,52 @@
+ 
+ //  1.
+ // const is the variable type for a variable that won't vary
+
+ // 2.
+ let num = 20;
+
+ // 3.
+ num = 15;
+
+ // 4.
+ num = num + 7;
+ console.log(num);
+
+ // 5.
+ num--;
+ console.log(num);
+
+ // 6.
+ num = "This is not a number";
+ console.log(num);
+
+// 7.
+let blank;
+
+//8.
+console.log(blank);
+
+// 9. 
+blank = null;
+console.log(blank);
+
+// Bonus 1.
+const  quarter = .25, dime = .1, nickel = .05, penny = .01;
+
+// Bonus 2.
+console.log(quarter, dime, nickel, penny);
+
+// Bonus 3.
+let letters = 'qwertyuiop';
+
+//Bonus 4.
+let pspot = 'qwertyuiop'.length;
+pspot--;
+console.log(pspot);
+console.log(letters[pspot]);
+
+
+
+
+
+
